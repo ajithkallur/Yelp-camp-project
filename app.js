d9972b49e17365e237f39eaf5c3ab60d57332844
Your Name <AJITH.GITAM@GMAIL.COM>
2023-07-18 14:48:13 +0530

    Ajith commit

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const cities = require('./Seeds/cities');
 const {places, descriptors} = require('./Seeds/seedhelpers')
 const morgan = require('morgan')
 const ejsMate = require('ejs-mate')
+const Joi = require('joi')
+const Schema = mongoose.Schema
+const catchAsync = require("./Utils/catchAsync");
+const ExpressError = require('./Utils/ExpressError');
+const Review = require('./models/reviews')
+ 
 var __dirname ="C:\Users\aajithkumarreddy\Desktop\Desktop\Personal\Full Stack\Yelp\Yelp camp project\views";
 main().catch(err => console.log(err));
 
@@ -19,7 +25,15 @@ const campGroundSchema = new mongoose.Schema({
   title: String,
     price: String,
     description: String,
-    location: String
+    location: String,
+    reviews:[
+
+      { 
+        type: Schema.Types.ObjectId,
+        ref: 'Review'
+
+      }
+    ]
 });
 app.listen(8080);
 console.log('Server is listening on port 8080');
@@ -48,12 +62,12 @@ const seedDB = async () => {
   for (let i = 0; i < 50; i++) {
       const random1000 = Math.floor(Math.random() * 1000);
       const price = Math.floor(Math.random() * 20) + 10;
-      const img = new Image(100, 200); // width, height
-       img.src = "https://picsum.photos/200/301"
+     // const img = new Image(100, 200); // width, height
+      // img.src = "https://picsum.photos/200/301"
       const camp = new Campground({
           location: `${cities[random1000].city}, ${cities[random1000].state}`,
           title: `${sample(descriptors)} ${sample(places)}`,
-          image: img,
+          image: "https://picsum.photos/200/301",
           description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
           price
       })
@@ -69,8 +83,28 @@ const seedDB = async () => {
  seedDB();
 // index page
 
- app.get('/', (req, res) => {
-  res.render('home');
+const validateCampground = (req,res,next) =>{
+ //server side validation with joi
+ const campgroundSchema = Joi.object({
+  campground:Joi.object({
+     title: Joi.string().required(),
+     location: Joi.string().required(),
+     price: Joi.number().required().min(0)
+  }).required()
+  })
+  
+  const {error} = campgroundSchema.validate(req.body)
+   if (error){
+    const msg = error.details.map(el=>el.message).join(',')
+    throw new ExpressError(msg,400)
+  }
+  else{
+    next()
+  }
+}
+ app.get('/', async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', {campgrounds});
 });
 
 app.get('/campgrounds',  async (req, res) => {
@@ -78,42 +112,66 @@ const campgrounds = await Campground.find({});
 res.render('campgrounds/index', {campgrounds});
 })
 
-app.post('/campgrounds',  async (req, res) => {
+app.post('/campgrounds',  validateCampground,catchAsync(async (req, res,next) => {
+  //if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+
+ 
   const campground1 = new Campground(req.body.campground)
   await campground1.save();
   //res.send("Succ save");
    res.redirect('/campgrounds')
-    });
+  
+    }));
     
+
+    app.post('/campgrounds/:id/reviews',  catchAsync(async (req, res,next) => {
+      const campground = await Campground.findById(req.params.id);
+      const review = new Review(req.body.review);
+      campground.reviews.push(review);
+      await review.save();
+      await campground.save();
+      res.redirect('/campgrounds/${campground._id}');
+    }))
 
 app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new');
 });
 
-app.get('/campgrounds/:id', async(req, res) => {
+app.get('/campgrounds/:id', catchAsync(async(req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/show', {campground});
-  })
+  }))
   app.post('/campgrounds/:id/edit',  async (req, res) => {
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
     res.redirect('/campgrounds');
       });
 
-app.get('/campgrounds/:id/edit', async(req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async(req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/edit', {campground});
-}); 
+})); 
 
 
-app.post('/campgrounds/:id/delete', async(req, res) => {
+app.post('/campgrounds/:id/delete', catchAsync(async(req, res) => {
   const {id} = req.params;
     const campground = await Campground.findByIdAndDelete(id)
     res.redirect('/campgrounds');
-}); 
+})); 
 
+//this invokes for any route which is not present
 //send 404 for any other request
-app.use((req,res) => {
-  res.status(404).send("NOT FOUND")
+app.all('*', (req,res,next) =>{
+next(new ExpressError('Page Not Found',404))
+})
+
+
+app.use((err,req,res,next) => {
+//destructure error
+const {statusCode =500} = err;
+if(!err.message){
+  err.message ="Something went wrong"
+}
+res.status(statusCode).render('error',{err});
 })
 
